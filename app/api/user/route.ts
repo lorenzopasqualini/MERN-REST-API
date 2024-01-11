3595e5a5a7de7cf1312c928501bb9ea5c66f43b8
Lorenzo Pasqualini <lorpasqu@gmail.com>
2024-01-12 00:40:39 +0100

    concatenate api + dbUsers, axios package

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,11 +1,17 @@
 import { NextRequest, NextResponse } from 'next/server';
 import User from "@/models/User";
 import dbConnect from "@/lib/dbConnect";
+import axios from 'axios';
 
 export async function GET(req: NextRequest, res: NextResponse){
   await dbConnect()
   try {
-    const allUsers = await User.find({})
+    const dbUsers = await User.find({})
+
+    const fetchApi = await axios.get('https://jsonplaceholder.typicode.com/users');
+    const tpUsers = fetchApi.data;
+
+    const allUsers = [...tpUsers, ...dbUsers];
     return NextResponse.json({ status: 200, message: 'success', data: allUsers })
   } catch (error) {
     return NextResponse.json({ status: 500, message: 'failure' })
